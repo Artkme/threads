6f1a2ec501ca710ed1cfa336a2a0b21a3c6db9cf
Saba Artkmeladze <saba.artkmeladze@gmail.com>
2025-07-28 22:57:53 +0400

    add task1

---
+++ src/main/java/subtask1/Version1.java
@@ -0,0 +1,42 @@
+package subtask1;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Version1 {
+    public static void main(String[] args) throws InterruptedException {
+        final ConcurrentHashMap<Integer, Integer> map = new ConcurrentHashMap<Integer, Integer>();
+
+        Runnable r1 = new Runnable() {
+            public void run() {
+                for (int i = 0; i < 1000; i++) {
+                    map.put(i, 1);
+                }
+            }
+        };
+
+        Runnable r2 = new Runnable() {
+            public void run() {
+                int sum = 0;
+                for (int val : map.values()) {
+                    sum += val;
+                }
+                System.out.println("Thread2: Sum is " + sum);
+            }
+        };
+
+        long startTime = System.nanoTime();
+
+        Thread t1 = new Thread(r1, "Writer");
+        Thread t2 = new Thread(r2, "Reader");
+        t1.start();
+        t1.join();
+        t2.start();
+        t2.join();
+
+        long endTime = System.nanoTime();
+        long durationInNano = endTime - startTime;
+        double durationInMillis = durationInNano / 1000000.0;
+
+        System.out.printf("Execution time: %.3f ms%n", durationInMillis);
+    }
+}
---
+++ src/main/java/subtask1/Version3.java
@@ -0,0 +1,43 @@
+package subtask1;
+
+import subtask1.collection.SynchronizedThreadSafeMap;
+
+public class Version3 {
+    public static void main(String[] args) throws InterruptedException {
+        final SynchronizedThreadSafeMap<Integer, Integer> map = new SynchronizedThreadSafeMap<Integer, Integer>();
+
+        Runnable r1 = new Runnable() {
+            public void run() {
+                for (int i = 0; i < 1000; i++) {
+                    map.put(i, 1);
+                }
+            }
+        };
+
+        Runnable r2 = new Runnable() {
+            public void run() {
+                int sum = 0;
+                for (int val : map.values()) {
+                    sum += val;
+                }
+                System.out.println("Thread2: Sum is " + sum);
+            }
+        };
+
+        long startTime = System.nanoTime();
+
+        Thread t1 = new Thread(r1, "Writer");
+        Thread t2 = new Thread(r2, "Reader");
+
+        t1.start();
+        t1.join();
+        t2.start();
+        t2.join();
+
+        long endTime = System.nanoTime();
+        long durationInNano = endTime - startTime;
+        double durationInMillis = durationInNano / 1000000.0;
+
+        System.out.printf("Execution time: %.3f ms%n", durationInMillis);
+    }
+}
